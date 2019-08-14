2f3c345711795bdd283223a422f9c18a5149eb5b
Yumi <ymym2103@gmail.com>
2019-08-14 11:06:16 +0800

    マイグレーションファイルの作成

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // 追加
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); // 追加
     }
 
     /**
