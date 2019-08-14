497ea61827b1dd3d4f4ce2a8b987c1b53c0e342b
Yumi <ymym2103@gmail.com>
2019-08-14 12:26:17 +0800

    デイリーコントローラー作成

---
+++ app/Http/Controllers/DiaryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DiaryController extends Controller
+{
+    // 追加
+    public function index()
+    {
+        return 'Hello World';
+    }
+}
