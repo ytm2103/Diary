36af73e91136e807a0a8f74b8a998c9cadb69f50
Yumi <ymym2103@gmail.com>
2019-08-15 11:02:34 +0800

    一覧を作成

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Diary;
 use Illuminate\Http\Request;
 
 class DiaryController extends Controller
@@ -9,6 +10,12 @@ class DiaryController extends Controller
     // 追加
     public function index()
     {
-        return 'Hello World';
+        //diariesテーブルのデータを全件取得
+        //useしてるDiaryのallメソッドを実施
+        //all()はテーブルのデータを全て取得するメソッド
+        $diaries = Diary::all(); 
+
+        // dd($diaries);  //var_dump()とdie()を合わせたメソッド。変数の確認 + 処理のストップ
+        return view('diaries.index',['diaries' => $diaries]);   
     }
 }
