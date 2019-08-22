854415108c6a676b6fe4c0b45f59ce36233c7674
Yumi <ymym2103@gmail.com>
2019-08-22 09:31:26 +0800

    テンプレート継承

--- resources/views/diaries/index.blade.php
+++ resources/views/diaries/index.blade.php
@@ -8,22 +8,31 @@
     <title>一覧表示画面</title>
 </head>
 <body>
+
+@extends('layout')
+
+@section('title')
+一覧
+@endsection
+
+@section('content')
 <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">
     新規投稿
 </a>
-    @foreach ($diaries as $diary)
-        <div class="m-4 p-4 border border-primary">
-            <p>{{ $diary->title }}</p>
-            <p>{{ $diary->body }}</p>
-            <p>{{ $diary->created_at }}</p>
-        </div>
-    @endforeach
-<p>{{ $diary->created_at }}</p>
-<a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
-<form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="POST" class="d-inline">
-    @csrf
-    @method('delete')
-    <button class="btn btn-danger">削除</button>
-</form>
+@foreach ($diaries as $diary)
+    <div class="m-4 p-4 border border-primary">
+        <p>{{ $diary->title }}</p>
+        <p>{{ $diary->body }}</p>
+        <p>{{ $diary->created_at }}</p>
+        <a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
+        <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="post" class="d-inline">
+            @csrf
+            @method('delete')
+            <button class="btn btn-danger">削除</button>
+        </form>
+    </div>
+@endforeach
+@endsection
+
 </body>
 </html>
