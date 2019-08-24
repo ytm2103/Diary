61586e29ad773b74fd07f28d548d667d71a2cd52
Yumi <ymym2103@gmail.com>
2019-08-24 17:11:53 +0800

    認証機能作成

---
+++ resources/views/diaries/edit.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>編集画面</title>
+</head>
+<body>
+@extends('layouts.app')
+    <section class="container m-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $message)
+                        <li class="alert alert-danger">{{ $message }}</li>
+                        @endforeach
+                    </ul>
+                @endif
+                <form action="{{ route('diary.update', ['id' => $diary->id]) }}" method="post">
+                    @csrf
+                    @method('put')
+                    <div class="form-group">
+                        <label for="title">タイトル</label>
+                        <input type="text" class="form-control" name="title" id="title" value="{{ old('title', $diary->title) }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="title">本文</label>
+                        <textarea class="form-control" name="body" id="body">{{ old('body', $diary->body) }}</textarea>
+                    </div>
+                    <div class="text-right">
+                        <button type="submit" class="btn btn-primary">更新</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
